f0ac5b8c1409ce45e7d5fb798c887c88ea2fee7f
Anneth Torres <torresaneth2@gmail.com>
2023-08-03 12:46:18 -0500

    prueba4657

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ export default function Card({id,name,status,species,gender,origin,image,onClose
    return (
       <div>
          <button onClick={onClose}>X</button>
-         <h2>Name: {name}</h2>
+         <h2>Name {name}</h2>
          <h2>Species: {species}</h2>
          <h2>Status: {status}</h2>
          <h2>Gender: {gender}</h2>
@@ -11,5 +11,3 @@ export default function Card({id,name,status,species,gender,origin,image,onClose
       </div>
    );
 }
-
-// este es un mensaje de prueba!

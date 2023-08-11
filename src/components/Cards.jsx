4e8f98ee57813056cb7f537c00a2b29f57e816f2
Anneth Torres <torresaneth2@gmail.com>
2023-08-11 12:37:28 -0500

    Practicando

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,23 +1,23 @@
 import Card from './Card';
 
-export default function Cards({characters}) {
+export default function Cards({ characters }) {
    return (
       <div>
          {
-            characters.map(({id,name,status,gender,species,image,origin})=>{
+            characters.map(({ id, name, status, species, gender, origin,
+               image }) => {
                return (
                   <Card
-                      key={id}
-                      id={id}
-                      name={name}
-                      status={status}
-                      gender={gender}
-                      species={species}
-                      image={image}
-                      origin={origin.name}
-                      onClose={() => alert('Emulamos que se cierra la card')}
-                  
-                  
+                     key={id}
+                     id={id}
+                     name={name}
+                     status={status}
+                     species={species}
+                     gender={gender} 
+                     image={image}
+                     origin={origin.name}
+                     onClose={() => alert('Emulamos que se cierra la card')}
+
                   />
                )
             })

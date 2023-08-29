57dc47fce2a035cb0365b7273fc3b9c2014d21c9
Anneth Torres <torresaneth2@gmail.com>
2023-08-29 12:29:20 -0500

    ROUTING RICK AND MORTY

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import './App.css';
 import Cards from './components/Cards.jsx';
 import Nav from "./components/Nav";
+import About from "./components/About";
+import Detail from "./components/Detail";
 import {useState} from "react";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
 
-const example = {
-   id: 1,
-   name: 'Rick Sanchez',
-   status: 'Alive',
-   species: 'Human',
-   gender: 'Male',
-   origin: {
-      name: 'Earth (C-137)',
-      url: 'https://rickandmortyapi.com/api/location/1',
-   },
-   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-};
 
 function App() {
    const [characters, setCharacters] = useState([]);
 
-   const onSearch =() =>{
-      setCharacters([...characters, example])
+   const onSearch=(id) => {
+      axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(response => response.data)
+      .then(( data ) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   const onClose=(id) =>{
+      const charactersFiltered = characters.filter(character => character.id !== id)
+      setCharacters(charactersFiltered)
    }
 
    return (
       <div className='App'>
          <Nav onSearch={onSearch}/>
-         <Cards characters={characters} />
+
+         <Routes>
+            <Route path="/home" element={ <Cards characters={characters} onClose={onClose} />} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/detail/:id" element={<Detail/>}/>
+         </Routes>
+
+
+         
+
          
       </div>
    );
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () =>{
+    return(
+        <div>
+            <h1>Work in progress!</h1>
+            <p>Hola, Soy Anneth</p>
+        </div>
+    )
+}
+
+export default About;
